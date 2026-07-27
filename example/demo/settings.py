9fe40cdb5828811e7ef4f43ffcdcac34277492fd
Rocco Roma <rococo034@gmail.com>
2026-07-27 21:32:11 +0200

    feat: add Dockerfile for example site and workflow to build, push and deploy

--- example/demo/settings.py
+++ example/demo/settings.py
@@ -19,13 +19,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+import os
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%yqjv945nma6r8@-g4277bhl73uop8s71bipc1!syz88))t087"
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-%yqjv945nma6r8@-g4277bhl73uop8s71bipc1!syz88))t087")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True").lower() in ("true", "1", "yes")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "djangoadminjs.info,localhost,127.0.0.1").split(",")
 
 
 # Application definition
